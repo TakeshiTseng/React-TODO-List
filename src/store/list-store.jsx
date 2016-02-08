a9c95c46209c7001a577aaf7cb50324e190983d5
Takeshi <a86487817@gmail.com>
2016-02-09 03:22:25 +0800

    Add constructor for todo list store

--- src/store/list-store.jsx
+++ src/store/list-store.jsx
@@ -3,6 +3,9 @@ import TodoListActions from '../action/list-action';
 let alt = require('../alt');
 
 class TodoListStore {
+  constructor() {
+    this.items = [];
+  }
 }
 
 module.exports = alt.createStore(TodoListStore, 'TodoListStore');
