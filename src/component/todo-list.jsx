1557db831757f93bb2ac708c1a10c90537fe9af8
Takeshi <a86487817@gmail.com>
2016-02-09 03:25:04 +0800

    Init component

---
+++ src/component/todo-list.jsx
@@ -0,0 +1,18 @@
+import TodoListStore from '../store/list-store';
+import TodoListActions from '../action/list-action';
+import React from 'react';
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+  }
+};
+class TodoList extends React.Component {
+  constructor(props) {
+  }
+  render() {
+  }
+};
+
+export default TodoList;
